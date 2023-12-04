1e78be4003d6c7dea5122e6bb9b619c897d57db6
clone 34435345io. 3 simple oi234 ioion23io n4 niooi 2 34 <152788646+eduffield9@users.noreply.github.com>
2023-12-04 17:50:50 -0600

    Update hirl.asm
    vooid io234o i nio23 noi4 noi23 ino4o i 234 iu io234 
    
    girl 2
    3 42
    3234 5 void io234 no23 o4 nio2iooi n2 oi io ionoi 234 
    
    h234 234. sapceoi2oin 34n io inoio 234

--- hirl.asm
+++ hirl.asm
@@ -1,3 +1,36 @@
+sapce oo n234n 2
+
+fodl 
+23 
+2 34  fold io23 n4io nio2ino  ino2 niooi  234 
+
+tiny heart oi23 nio4 noi234 ace em he o823 4ni oi23 oi4 oi234 io io23io n 23 4
+h 23
+4 
+234
+ 2 234 spaceooi 234oi oi2ionio n2 ino34 noioi23 io oi2 34 
+3423
+234  o2 io34 
+2n3234
+234  23 4
+4 23   vodi io234 ionio2i34  23
+
+heart 2 cell2 3o4ni o2in3oi n ino ion 234 
+34 2
+34 234o i 2oioi  234 heartoi2 n3o4no23n o4 noi23oin i n23 4
+ 2 3
+
+ 23
+
+234 heartlo23 o4 oin2ino234 
+
+23
+4  scoreio2 on34 o2 oi34 ion2i no34iooi 2oi inoio 234 
+234 
+234 
+   medium 2oi34n io23oi 4 ion23 ion4 io23oi4 io io 234 
+
+
 hi get coin2 34o oi23 nio4 nio23 oin 234 
 234
 234
