cd215934df10feb95e4ee84207595a65142d53a0
34435345io. 3 simple oi234 ioion23io n4 niooi 2 34 <152788646+eduffield9@users.noreply.github.com>
2023-12-04 17:07:59 -0600

    Create hirl.asm

---
+++ hirl.asm
@@ -0,0 +1,18 @@
+coin2 34o oi23 nio4 nio23 oin 234 
+234
+234
+
+2
+34234 deadfo2 3io4 nio2io 3 oi inooi 234 ao cort i23 ino4 32
+2#
+ 2334 234 death o234 io noioi n234 
+ 42
+234  fuck you2 j34io io23nioion 23 oino2i34 io23 4io io2io uoi 234 that igl io2 nio34 2 #
+$ 
+2234 oio2in 34no io i234 3
+3
+234 i2n io234 ino 2ion34 noioiu234  your io23 ino4ion2io 34 lfie io noi23o in4o in2io 34 io23 4
+ 2234
+344
+23
+2 3342 is o234 niooi 23 iouoi 2oi 23 death io234 oi no2i3 noi234
