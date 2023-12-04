7b6d437f200ada69cb2a0e3f952bc589753d1f42
34435345io. 3 <152788646+eduffield9@users.noreply.github.com>
2023-12-04 16:33:10 -0600

    Update and rename asm to asmitbinaro.asm

---
+++ asmitbinaro.asm
@@ -0,0 +1,31 @@
+less
+more ;essop op234pon np23 4no noi234o n no2i34o ni noi234
+
+ha
+h
+ha tio2 3948 8923 u4in2 3io4 nion io noi234
+ha
+  more io23io4  ino23 ino4 ion234 io 2noi34 oin2 on3i4 on2 3no4 oin234 
+h
+ah
+h more o823489  89234in 23ooi  234 lessoi2 3no4 iop23 io4 oi23 oin4no i23 noi4n iooin  234 
+ah
+  less more oi23 io4 ino234 ino2 ino34 ino23 ion4 oin23 no4 noi23 noi4 ino o23 4oin ip2 io23o4 io no2 oi34 oi2 34 
+
+it as29349293492938 42 3o4n 23 o234 o2 34o oi2 o234 
+j2
+ 34 
+234  hi 898 899oi23ioion2io n3noi 2o ino ni23o nio ni23o n4 noi23io noi 234 
+
+ah  get 2349239 8423io4 nio234no i2no i34 nio2 nio34ino  n2io34 nio2 oin34i on234 
+ha  
+h
+ha i 3oi oi 3oi4 ion5 io get win noi23 4io 234 wrieoi23 4io oin23io423io 4oiio 234  witre les io2 ip34234 
+ha
+ha
+ha form 2o34 op 2o nio23n io4io n2noi34noi n io2234 
+ah
+ha orgol2 3io4o n2no i234 
+ah
+ah
+ah orglo2 3io4o 2o no i2no oni23oin4n oi23 nio4 oi23oin 234 
